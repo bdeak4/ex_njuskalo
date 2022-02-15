9bd80ab26a24dcf5395cfae78f7942df78c3f2f9
Bartol Deak <b@bdeak.net>
2022-02-16 00:42:18 +0100

    add search_ads fn

--- lib/ex_njuskalo.ex
+++ lib/ex_njuskalo.ex
@@ -5,8 +5,52 @@ defmodule ExNjuskalo do
 
   @doc """
   Returns search results
+
+  Possible values for sort:
+
+  - `relevance` (Relevantnost)
+  - `old` (Najstariji)
+  - `new` (Najnoviji)
+  - `expensive` (S višom cijenom)
+  - `cheap` (S nižom cijenom)
   """
-  def search_ads(query) do
+  def search_ads(query), do: search_ads(query, nil, "relevance")
+  def search_ads(query, category_id), do: search_ads(query, category_id, "relevance")
+
+  def search_ads(query, category_id, sort) do
+    qs =
+      %{
+        search: query,
+        include: "adListItems.image.variations",
+        rootCategoryId: 0
+      }
+      |> maybe_put(category_id != nil, "categoryId[#{category_id}]", 1)
+      |> maybe_put(sort != nil, "sort[#{sort}]", 1)
+      |> URI.encode_query()
+
+    resp = get_resp("mobile/page-ad-list?" <> qs)
+
+    if Map.has_key?(resp, "errors"), do: IO.inspect(resp)
+
+    %{
+      ads:
+        resp["data"]["relationships"]["adListItems"]["data"]
+        |> Enum.map(fn a ->
+          ad =
+            Enum.find(resp["included"], fn i ->
+              i["id"] == a["id"] && i["type"] == "ad-list-item"
+            end)
+
+          image =
+            Enum.find(resp["included"], fn i ->
+              i["id"] == "#{ad["relationships"]["image"]["data"]["id"]}-variation-240x320"
+            end)
+
+          Map.put(ad, "image", image)
+        end),
+      categories: resp["meta"]["aggregations"]["categoryId"]["groups"],
+      total_ads: resp["meta"]["totalAdCount"]
+    }
   end
 
   @doc """
@@ -22,7 +66,7 @@ defmodule ExNjuskalo do
     |> Enum.map(fn r ->
       %{
         label: r["attributes"]["label"],
-        group_id: String.to_integer(r["id"])
+        category_id: String.to_integer(r["id"])
       }
     end)
   end
@@ -44,9 +88,9 @@ defmodule ExNjuskalo do
     |> Jason.decode!()
   end
 
-  defp get_resp_with_auth(username, password, path) do
-    :todo
-  end
+  # defp get_resp_with_auth(path) do
+  #   :todo
+  # end
 
   defp get_token(formdata) do
     api_url("token")
@@ -74,4 +118,9 @@ defmodule ExNjuskalo do
       "cap" <>
       "i/v2/" <> path
   end
+
+  defp maybe_put(map, _key, nil), do: map
+  defp maybe_put(map, key, value), do: Map.put(map, key, value)
+  defp maybe_put(map, false, _key, _value), do: map
+  defp maybe_put(map, true, key, value), do: maybe_put(map, key, value)
 end
